c29e2cf8e40c63441bed17d23d0c55f00b0c55be
Dennis <dmyasnyankin@berkeley.edu>
2019-01-02 04:51:20 -0800

    Set up routes and import relevant info to app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+const tweets = require("./routes/api/tweets");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -10,5 +13,13 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
